eaf2098764b0733caf7fb08ac90555d0a1b6ce78
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2012-07-20 16:52:16 +0200

    pylib: add StatList

--- kkstats.py
+++ kkstats.py
@@ -16,3 +16,45 @@ def deviation(data):
 	m = mean(data)
 	dev = sqrt(float(sum(( (m-d)**2 for d in data )))/len(data))
 	return dev
+
+class StatList(list):
+	"""
+	Overload a list, adding some simple statistics functions
+
+	It allows the user to define a function to map the elements of the list,
+	before performing the calculations
+
+	Example:
+	>>> x = StatList((10,10,11,12))
+	>>> print x.samples, x.avg
+	4 10.75
+	>>> x = StatList((10,10,11,12), fn=lambda x: x/10.0)
+	>>> print x.samples, x.avg
+	4 1.075
+
+	"""
+	def __init__(self, l=[], fn=None):
+		list.__init__(self, l)
+		if fn is None:
+			fn = lambda x: x
+		self._fn = fn
+
+	@property
+	def max(self):
+		return max(map(self._fn,self))
+
+	@property
+	def min(self):
+		return min(map(self._fn,self))
+
+	@property
+	def avg(self):
+		return mean(map(self._fn,self))
+
+	@property
+	def samples(self):
+		return len(self)
+
+	@property
+	def dev(self):
+		return deviation(map(self._fn,self))
