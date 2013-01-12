06a0ae37436ea42cc24967ea992bf45c86976f20
Kornilios Kourtis <kkourt@inf.ethz.ch>
2013-01-12 18:04:20 +0100

    dict_hier: dhier_reduce_many()

--- dict_hier.py
+++ dict_hier.py
@@ -11,8 +11,17 @@ def dhier_reduce(lod, key, nonexistant=None):
 		ret[v].append(d)
 	return {key: ret}
 
-def dhier_reduce_many(lod, *keys):
-	pass
+def dhier_reduce_many(lod, keys, map_fn=None, nonexistant=None):
+
+	if len(keys) == 0:
+		return  map_fn(lod) if map_fn is not None else lod
+
+	k = keys[0]
+	ret = {}
+	d = dhier_reduce(lod, k, nonexistant)[k]
+	for xk, xd in d.iteritems():
+		ret[xk] = dhier_reduce_many(xd, keys[1:], map_fn, nonexistant)
+	return ret
 
 def dhier_iterate_leafs(d):
 	for v in d.itervalues():
