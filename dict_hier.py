453c18f2188d9c72a69d20d507a5b3322e7b4ff4
Kornilios Kourtis <kkourt@inf.ethz.ch>
2013-02-04 00:20:17 +0100

    dhier_reduce_many2: (hopefully) temporary uglines

--- dict_hier.py
+++ dict_hier.py
@@ -23,6 +23,35 @@ def dhier_reduce_many(lod, keys, map_fn=None, nonexistant=None):
 		ret[xk] = dhier_reduce_many(xd, keys[1:], map_fn, nonexistant)
 	return ret
 
+## returns a tuple of
+#  ret1: a hierarchical dict with key=parameter value 
+#                                 val=hiearchical dict or data list
+#  ret2: a dict with key->set of parameters
+def dhier_reduce_many2(lod, keys, map_fn=None, nonexistant=None):
+
+	if len(keys) == 0:
+		ret1 =  map_fn(lod) if map_fn is not None else lod
+		ret2 = {}
+		return (ret1, ret2)
+
+	k = keys[0]
+	ret1  = {}
+	ret2v = []
+	ret2  = {}
+	d = dhier_reduce(lod, k, nonexistant)[k]
+	for xk, xd in d.iteritems():
+		ret1[xk], ret2_prev = dhier_reduce_many2(xd, keys[1:], map_fn, nonexistant)
+		ret2v.append(xk)
+		for param_key, param_set in ret2_prev.iteritems():
+			if param_key not in ret2:
+				ret2[param_key] = param_set
+			else:
+				ret2[param_key] = ret2[param_key].union(param_set)
+
+	assert k not in ret2
+	ret2[k] = set(ret2v)
+	return (ret1, ret2)
+
 def dhier_iterate_leafs(d):
 	for v in d.itervalues():
 		if isinstance(v, types.DictType):
